5cf8a3564150aa3ae6e83a56bdc9a140fc84d94f
suzuki_kiwamu <suzuki_kiwamu@cyberagent.co.jp>
2014-11-11 16:16:46 +0900

    Initial Commit

---
+++ ui-sample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ui-sample
+//
+//  Created by suzuki_kiwamu on 11/11/14.
+//  Copyright (c) 2014 suzuki_kiwamu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ui-sample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ui-sample
+//
+//  Created by suzuki_kiwamu on 11/11/14.
+//  Copyright (c) 2014 suzuki_kiwamu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
